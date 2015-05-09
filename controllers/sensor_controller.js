10e1e4618d4a4326845f7dbedc7fcd112bf5fb16
mariagf <maria.garcia.fdez.mgf@gmail.com>
2015-05-09 22:12:39 +0200

    2.7

--- controllers/sensor_controller.js
+++ controllers/sensor_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 // GET /sensores
 exports.index = function(req,res){
-	models.Quiz.findAll().then(function(sensores){
+	models.Sensor.findAll().then(function(sensores){
 		res.render('sensores/index',{sensores: sensores});
 	})
 	
